6ff3fd3a1a23cdf7aab3f26d01c7a78f8a928309
hossamDeveloper <hoss.soliman45@gmail.com>
2025-05-14 12:05:53 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import FlowCalculate from "./components/FlowCalculate";
+import FlowCalculate from "./components/FlowCalculate.jsx";
 
 function App() {
   return (
